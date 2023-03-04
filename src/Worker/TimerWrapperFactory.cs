47a8ed09d93a838016f6e2175e2ffdf29c5a52bb
David Basarab <dbasarab617@gmail.com>
2023-03-04 13:54:38 -0500

    created timer wrapper factory

---
+++ src/Worker/TimerWrapperFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FatCat.Worker;
+
+internal interface ITimerWrapperFactory
+{
+	ITimerWrapper CreateTimerWrapper();
+}
+
+[ExcludeFromCodeCoverage(Justification = "")]
+internal class TimerWrapperFactory : ITimerWrapperFactory
+{
+	public ITimerWrapper CreateTimerWrapper() => new TimerWrapper();
+}
