ec206130413f40d96a8051cc7daf1a844e785ba6
David Basarab <dbasarab617@gmail.com>
2023-03-06 18:50:14 -0500

    done on timer worker item start

--- src/Tests.Worker/TimerWorkerItemSpecs/StartTests.cs
+++ src/Tests.Worker/TimerWorkerItemSpecs/StartTests.cs
@@ -35,6 +35,16 @@ public class StartTests
 	}
 
 	[Fact]
+	public void OnlyStartTheTimeOnce()
+	{
+		timerWorkerItem.Start(workerItem);
+		timerWorkerItem.Start(workerItem);
+
+		A.CallTo(() => timerWrapper.Start())
+		.MustHaveHappenedOnceExactly();
+	}
+
+	[Fact]
 	public void SetAutoResetOnTimer()
 	{
 		timerWorkerItem.Start(workerItem);
@@ -42,6 +52,35 @@ public class StartTests
 		timerWrapper.AutoReset
 					.Should()
 					.Be(!waitForDelay);
+	}
+
+	[Fact]
+	public void SetIntervalOnTimer()
+	{
+		timerWorkerItem.Start(workerItem);
+
+		timerWrapper.Interval
+					.Should()
+					.Be(interval);
+	}
+
+	[Fact]
+	public void SetTimerElapsedFunction()
+	{
+		timerWorkerItem.Start(workerItem);
+
+		timerWrapper.OnTimerElapsed
+					.Should()
+					.NotBeNull();
+	}
+
+	[Fact]
+	public void StartTimer()
+	{
+		timerWorkerItem.Start(workerItem);
+
+		A.CallTo(() => timerWrapper.Start())
+		.MustHaveHappened();
 	}
 
 	private void SetUpTimerWrapperFactory()
--- src/Worker/TimerWorkerItem.cs
+++ src/Worker/TimerWorkerItem.cs
@@ -24,37 +24,25 @@ internal class TimerWorkerItem : ITimerWorkerItem
 
 	public void Start(IWorkerItem workerItem)
 	{
-		ConsoleLog.WriteGreen("TEMP");
-		ConsoleLog.WriteGreen("TEMP");
-		
 		this.workerItem = workerItem;
-		//
-		// if (timer != null) return;
-		//
+
+		if (timer != null) return;
+
 		timer = timerWrapperFactory.CreateTimerWrapper();
-		
+
 		timer.AutoReset = !this.workerItem.WaitOnWorkBeforeDelay();
-		// timer.Interval = this.workerItem.Interval;
-		//
-		// timer.OnTimerElapsed += TimerElapsed;
-		//
-		// var looking = 13;
-		//
-		// if (timer.AutoReset)
-		// {
-		// 	looking = 13;
-		// }
-		//
-		// ConsoleLog.WriteMagenta($"Looking at {looking}");
-		//
-		// timer.Start();
+		timer.Interval = this.workerItem.Interval;
+
+		timer.OnTimerElapsed = TimerElapsed;
+
+		timer.Start();
 	}
 
 	public void Stop()
 	{
 		// TODO : Unit Test
 		// timer?.Dispose();
-		
+
 		ConsoleLog.WriteGreen("TEMP");
 		ConsoleLog.WriteGreen("TEMP");
 	}
@@ -62,11 +50,11 @@ internal class TimerWorkerItem : ITimerWorkerItem
 	public void TimerElapsed()
 	{
 		// TODO : Unit Test
-		
+
 		// workerItem.DoWork().Wait();
 		//
 		// if (!timer.AutoReset) timer.Start();
-		
+
 		ConsoleLog.WriteGreen("TEMP");
 		ConsoleLog.WriteGreen("TEMP");
 	}
